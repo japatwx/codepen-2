f6df724befd9f61e7e13636a6a5e776101147591
japatwx <127530520+japatwx@users.noreply.github.com>
2023-06-01 12:30:42 -0300

    1

--- java.js
+++ java.js
@@ -1,20 +1,15 @@
-const restartButton = document.querySelector(".reset");
+import { neonCursor } from 'https://unpkg.com/threejs-toys@0.0.8/build/threejs-toys.module.cdn.min.js'
 
-restartButton.addEventListener(
-    "click",
-    () => {
-        const textAnimation = document.querySelector(".text-stroke");
-
-        setAnimationName(textAnimation, "none");
-        requestAnimationFrame(() =>
-            setTimeout(() => setAnimationName(textAnimation, ""), 0)
-        );
-    },
-    false
-);
-
-const setAnimationName = (element, animationName) => {
-    if (element) {
-        element.style.animationName = animationName;
-    }
-};
+neonCursor({
+  el: document.getElementById('app'),
+  shaderPoints: 16,
+  curvePoints: 80,
+  curveLerp: 0.5,
+  radius1: 5,
+  radius2: 30,
+  velocityTreshold: 10,
+  sleepRadiusX: 100,
+  sleepRadiusY: 100,
+  sleepTimeCoefX: 0.0025,
+  sleepTimeCoefY: 0.0025
+})
